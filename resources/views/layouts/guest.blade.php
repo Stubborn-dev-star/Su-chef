8f41d3b410fac9ade25141d693a3fda5e272775f
David Charles <kingdavidchris01@gmail.com>
2026-06-03 06:14:40 +0100

    fixed Login and Signup page and adjusted system components

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center backdrop-blur-sm bg-[url('https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=1600')]">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-20 h-20 fill-current text-secondary" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 z-0 bg-transparent/10 backdrop-blur-sm shadow-5xl overflow-hidden sm:rounded-lg border border-b-4 border-blac">
                 {{ $slot }}
             </div>
         </div>
